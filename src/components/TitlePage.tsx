4c09ed49aa3a844fb93e5dc1e4d51ef0ff78264b
juuck14 <juuck14@gmail.com>
2022-11-24 20:39:31 +0900

    error fix

--- src/components/TitlePage.tsx
+++ src/components/TitlePage.tsx
@@ -8,10 +8,12 @@ const TitlePage = ({animationEnd} : any) => {
                 <div className="title title1">
                     <div className="titleText">안녕하세요.</div>
                     <div className="box"></div>
+                    <div className="bar"></div>
                 </div>
                 <div className="title title2" onAnimationEnd={animationEnd}>
                     <div className="titleText">개발자 박서진입니다.</div>
                     <div className="box"></div>
+                    <div className="bar"></div>
                 </div>
             </div>
         </div>
